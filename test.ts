15290c075eff9aa3bad8ff913791e6098b2d6bc7
mygizli04 <42012824+mygizli04@users.noreply.github.com>
2023-01-04 01:48:29 +0400

    Remove test.ts
    Did not mean to commit that file

--- test.ts
+++
@@ -1,7 +0,0 @@
-import { spinTheWheel } from "./wheel/index.js";
-import { setTimeout as sleep } from "timers/promises";
-
-while (true) {
-    await sleep(500);
-    console.log(spinTheWheel("sbeveuwu"));
-}
